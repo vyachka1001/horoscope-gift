0deef47f49b77a2511be5100d102d65d67ac3cd6
harvidston <harvidston@mail.ru>
2022-03-24 01:15:30 +0300

    main section is ready

--- client/src/components/main-section/index.js
+++ client/src/components/main-section/index.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import Video from '../../videos/video.mp4'
-import { MainContainer, MainBg, VideoBg } from './main-section_elements'
+import React, {useState} from 'react'
+import Video from '../../videos/white.mp4'
+import {Button} from '../button_element'
+import { MainContainer, MainBg, VideoBg, MainSectionContent, MainH1, MainP, MainBtnWrapper, ArrowForward, ArrowRight } from './main-section_elements'
 
 const MainSection = () => {
+  const [hover, setHover] = useState(false)
+  const onHover = () => {
+    setHover(!hover)
+  }
+
   return (
     <MainContainer>
       <MainBg>
         <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
       </MainBg>
+      <MainSectionContent>
+        <MainH1>Horoscope gift</MainH1>
+        <MainP>
+          Find out by the stars what kind of rest you need
+        </MainP>
+        <MainBtnWrapper>
+          <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover} primary='true' dark='true' >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>  
+        </MainBtnWrapper>
+      </MainSectionContent>
     </MainContainer>
   )
 }
