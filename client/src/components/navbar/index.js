6d34b28a2d0aff452ea9ccd7572843a69bc15b32
harvidston <harvidston@mail.ru>
2022-03-23 13:54:19 +0300

    added pages

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -3,13 +3,13 @@ import {FaBars} from 'react-icons/fa'
 import { Nav, NavbarContainer, NavLogo, NavMenu, MobileIcon, NavItem, NavLinks, NavBtn, NavBtnLink} from './navbar_elements'
 
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
 return (
    <>   
    <Nav>
      <NavbarContainer>
       <NavLogo to='/'>horoscope</NavLogo>
-      <MobileIcon>
+      <MobileIcon onClick={toggle}>
         <FaBars />
       </MobileIcon>
       <NavMenu>
