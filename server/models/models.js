7983522296f1243257b311ad4c964094c85722a0
harvidston <harvidston@mail.ru>
2022-03-29 14:21:43 +0300

    added connection to the database, data models and  relations between them

---
+++ server/models/models.js
@@ -0,0 +1,66 @@
+const sequelize = require('../database')
+const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('user',{
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, primaryKey: true, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: "USER"}
+})
+
+const Basket = sequelize.define('basket',{
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+})
+
+const BasketCertificate = sequelize.define('basket-certificate',{
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
+})
+
+const Certificate = sequelize.define('certificate',{
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    price: {type: DataTypes.INTEGER, allowNull: false},
+    img: {type: DataTypes.STRING, allowNull: false},
+    rating: {type: DataTypes.INTEGER, defaultValue: 0}
+})
+
+const CatalogCategory = sequelize.define('catalog-category',{
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false}
+})
+
+const CertificateInfo = sequelize.define('certificate-info',{
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, unique: true, allowNull: false},
+    description: {type: DataTypes.STRING, allowNull: false}
+})
+
+const Rating = sequelize.define('rating',{
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type: DataTypes.INTEGER , allowNull: false}
+})
+
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+User.hasMany(Rating)
+Rating.belongsTo(User)
+
+Basket.hasMany(BasketCertificate)
+BasketCertificate.belongsTo(Basket)
+
+Certificate.hasMany(BasketCertificate)
+BasketCertificate.belongsTo(Certificate)
+
+Certificate.hasMany(Rating)
+Rating.belongsTo(Certificate)
+
+CatalogCategory.hasMany(Certificate)
+Certificate.belongsTo(CatalogCategory)
+
+Certificate.hasOne(CertificateInfo)
+CertificateInfo.belongsTo(Certificate)
+
+module.exports = {
+    User, Basket, BasketCertificate, Certificate, CertificateInfo, CatalogCategory, Rating
+}
--- server/server.js
+++ server/server.js
@@ -1,4 +1,28 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const sequelize = require('./database')
+const models = require('./models/models')
+const cors = require('cors')
 
-app.listen(5000, () => console.log ('Server started on port 5000...'))
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Working !!!'})
+})
+
+const start = async () => {
+    try{
+        await sequelize.authenticate()
+        await sequelize.sync()
+
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start() 
