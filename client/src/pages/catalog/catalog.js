e99f1722535c3c65d60a2523ba4048c9165d5515
harvidston <harvidston@mail.ru>
2022-04-01 13:23:33 +0300

    Add catalog & personal certificate pages

---
+++ client/src/pages/catalog/catalog.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import CatalogPage from '../../components/catalog'
+
+
+
+const Catalog = () => {
+//   const [isOpen, setIsOpen] = useState(false)
+//   const toggle = () => {
+//     setIsOpen(!isOpen)
+//   };
+  
+  return (
+    <>
+       <CatalogPage />
+    </>
+  )
+}
+
+export default Catalog
---
+++ client/src/pages/catalog/certificate-page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CertificatePersonalPage from '../../components/certificate-pages/certificate-personal-page'
+
+
+const CertificatePage = () => {
+  return (
+      <CertificatePersonalPage />
+  )
+}
+
+export default CertificatePage
