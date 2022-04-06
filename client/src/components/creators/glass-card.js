453b676dbc80f2e19c39316aaf109506b3e347f5
harvidston <harvidston@mail.ru>
2022-04-06 04:19:49 +0300

    Creators page fully implemented

--- client/src/components/creators/glass-card.js
+++ client/src/components/creators/glass-card.js
@@ -1,73 +1,100 @@
 import React from 'react'
 import styled from 'styled-components'
-import profile from '../../images/clients/user.svg'
-import { useSpring, animated, config} from 'react-spring'
+import Alexey from '../../images/creators/user.svg'
+import Slava from '../../images/creators/user.svg'
+import { useSpring, animated } from 'react-spring'
 
 
+export const Container = styled.div`
 
-export const GlassCard = styled.styled(animated.div)`
-// export const GlassCard = styled.div
+`
+export const GlassCard = styled(animated.div)`
     display: inline-block;
-    // padding: 3em;
-    background: #C7D2FE66;
+    // background: #C7D2FE66;
+    //  background: #F0F1F8;
     border-radius: 30px;
     z-index: 1;
     position: relative;
-    backdrop-filter: blur(5px);
-    border: 2px solid transparent;
+    // backdrop-filter: blur(3px);
+    // border: 1px solid #B762C1;
+    border: 1px solid transparent;
+
     background-clip: border-box;
     cursor: pointer;
     height: 400px;
     width: 400px;
     text-align: center;
+    margin-right: 50px;
+    margin-left: 50px;
+
 `
 
 export const CreatorImage = styled.img`
-    width: 200px;
-    height: auto;
-    border: 1px solid #000;
+    width: 220px;
+    height: 220px;
+    border: 1px solid #B762C1;
     border-radius: 50%;
     margin-top: 50px ;
-    margin-left: 100px;
-    margin-bottom: 20px;
+    margin-left: 90px;
+    margin-bottom: 30px;
     
 `
 export const CardH1 = styled.h1`
    line-heright: 1.5;
    letter-spacing: 1.5;
-   color: #000;
+   color: #fff;
     
 `
 export const CardH3 = styled.h3`
     line-heright: 1.5;
     letter-spacing: 1.5;
     font-size: 16px;
-    color: #000;
+    color: #B762C1;
 
 `
 
 const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1]
-
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s}) `
 
-const GlassCardPage = () => {
 
-    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config:{ mass: 10, tension: 200, friction: 50 } }))
+const calc2 = (a, b) => [-(b - window.innerHeight / 2) / 20, (a - window.innerWidth / 2) / 20, 1]
+const trans2 = (a, b, c) => `perspective(600px) rotateX(${a}deg) rotateY(${b}deg) scale(${c}) `
+
+const GlassCardsPage = () => {
+
+    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config:{ mass: 8, tension: 250, friction: 8 }, abc: [0, 0, 1], config:{ mass: 8, tension: 250, friction: 8 }}))
+
 
   return (
-    <GlassCard
-        onMouseMove={({clientX: x, clientY: y}) => (set({xys: calc(x, y)}))}
-        onMouseLeave={() => set ({xys: [0,0,1]})}
-        style={{ 
-            transform: props.xys.interpolate(trans)
-        }}
-    >
-        <CreatorImage src={profile} />
-        <CardH1>Alexey Zaitsev</CardH1>
-        <CardH3>Full Stack Developer</CardH3>
+    <Container>
+        <GlassCard
+            onMouseMove={({clientX: x, clientY: y}) => (set({xys: calc(x, y)}))}
+            onMouseLeave={() => set ({xys: [0,0,1]})}
+            style={{ 
+                transform: props.xys.interpolate(trans)
+            }}
+        >
+            <CreatorImage src={Alexey} />
+            <CardH1>Alexey Zaitsev</CardH1>
+            <CardH3>Full Stack Developer</CardH3>
 
-    </GlassCard>
+            
+        </GlassCard>
+        <GlassCard
+            onMouseMove={({clientX: a, clientY: b}) => (set({abc: calc2(a, b)}))}
+            onMouseLeave={() => set ({abc: [0,0,1]})}
+            style={{ 
+                transform: props.abc.interpolate(trans2)
+            }}
+        >
+            <CreatorImage src={Slava} />
+            <CardH1>Viachaslau Viarbitski</CardH1>
+            <CardH3>Full Stack Developer</CardH3>
+
+            
+        </GlassCard>
+    </Container>
   )
 }
 
-export default GlassCardPage
+export default GlassCardsPage
--- client/src/components/creators/index.js
+++ client/src/components/creators/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import Video from '../../videos/creators.mp4'
-import Image from '../../images/clients/1.jpg'
-import profile from '../../images/clients/user.svg'
-import GlassCardPage from './glass-card'
+import Video from '../../videos/space.mp4'
+import Image from '../../images/creators/1.jpg'
+import GlassCardsPage from './glass-card'
 
 
 export const FormImage = styled.div`
@@ -19,19 +18,44 @@ export const FormImage = styled.div`
 `
 
 export const Container = styled.div`
-   
+    width: 100%;
+    height: 900px;
+    background-position: center;
+    background-size: cover;
+    background-repeat: 'no-repeat';
+    display: flex;
+    align-items: center;
+    justify-content: center;
     
+`
+export const MainBg = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+`
+export const VideoBg = styled.video`
+    width: 100%;
+    height: 100%;
+    -o-object-fit: cover;
+    object-fit: cover;
+    background: #232a34;
 `
 
 
 const CreatorsPage = () => {
   return (
       <>
-       <Container> 
-             <FormImage>
-                <GlassCardPage />
-             </FormImage> 
-         </Container> 
+        <Container> 
+            <MainBg>
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+            </MainBg>
+            <GlassCardsPage/>
+        </Container>  
       </>
   )
 }
