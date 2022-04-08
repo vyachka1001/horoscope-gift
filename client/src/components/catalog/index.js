e12224be8cc9565fc8bb3a15c8a111e532e4a425
harvidston <harvidston@mail.ru>
2022-04-08 03:35:57 +0300

    Add certificate items

--- client/src/components/catalog/index.js
+++ client/src/components/catalog/index.js
@@ -43,7 +43,6 @@ const CatalogPage = observer (() => {
         <CatalogContainer>
             <CNavbar />
             {/* <Sidebar /> */}
-
             {/* <Slider /> */}
             {/* <TypeBar /> */}
 
