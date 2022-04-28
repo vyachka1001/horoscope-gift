a250742fa6b0496898be7612e0665201c3fe7c00
Viachaslau Viarbitski <viachaslau@vyachka.laptop>
2022-04-28 14:29:07 +0300

    impl register function

--- server/controllers/controller-user.js
+++ server/controllers/controller-user.js
@@ -1,9 +1,44 @@
-const ApiError = require ('../error/api-error')
+const ApiError = require ('../error/api-error');
+const {User} = require ('../models/models');
+const {check, validationResult} = require ('express-validator');
+const bcrypt = require ('bcryptjs');
+
 class userController {
-    async registration (req, res){
+    async register (req, res){
+        try {
+            let errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    message: "Incorrect operation",
+                    errors
+                });
+            }
+
+            const {email, password} = req.body;
+            const candidate = User.findOne(email);
+            if (candidate) {
+                return res.status(400).json({message: `User with email ${email} already exist`});
+            }
+
+            //somehow create salt
+            let salt = 'testing_salt';
+            let hashPassword = await bcrypt.hash(password, salt);
+            User.create({
+                email: email,
+                password: hashPassword
+            });
+
+            return res.json({message: "User was created"});
+
+        } catch (e) {
+            console.log(e);
+            res.send({message: `Server error`});
+        }
     }
+
     async login (req, res){
     }
+
     async check (req, res, next){
          const {id} = req.query;
          if (!id){
